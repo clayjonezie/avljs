1beefb2502d65b7d4545d9ac6c603870d42da8eb
Clay Jones <clay.mark.jones@gmail.com>
2013-10-25 00:37:46 -0700

    got testing sorted out eh

--- tests.js
+++ tests.js
@@ -1,43 +1,36 @@
 // some tests using said framework go here
-//
-//
 
-var AVL = require("./avl.js");
+var AVL = require("./avl.js"),          // the module we are testng
+    cj  = require("./lib/cjTest.js");   // modestly named testing module
+
 var avl = new AVL();
 
-var t0 = function() {
-
+var testf = function test1() {
     avl.add(3);
-    return avl.height() === 0;
-}
+    avl.add(-7);
+    avl.add(20);
+    avl.add(25);
+    avl.add(1);
+    avl.add(0);
+    cj.test(avl.height(), 3);
+}();
 
 var t1 = function() {
     avl.add(4);
-    return avl.height() === 1;
+    cj.test(avl.height, 3);
 }
 
 var t2 = function() {
     avl.remove(4);
-    return avl.height === 0;
+    cj.test(avl.height, 0);
 }
 
 var t3 = function() {
-    return avl.contains(3);
+    cj.test(avl.contains(3), true);
 }
 
 var t4 = function() {
-    return !avl.contains(22);
+    cj.test(avl.contains(22), false);
 }
 
-var tests = [t0, t1, t2, t3, t4];
-
-tests.forEach(function(test, index) {
-    if (!test()) {
-        console.log("⛔  failed " + index);
-//        console.log(test.toString());
-    } else
-        console.log("   passed " + index);
-
-});
-
-
+cj.run();
